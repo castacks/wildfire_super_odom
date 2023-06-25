72b979fdb0544b022eee597cc923a9d79771b3ba
Yuanjun Gao <yuanjung@andrew.cmu.edu>
2023-06-25 00:49:30 -0400

    more elegant implementation
    pass environment variable to tmux yaml file for localization

--- operations/batch_process_local.py
+++ operations/batch_process_local.py
@@ -19,13 +19,14 @@ if __name__ == "__main__":
     parser.add_argument("configuration_file", help="JSON configuration file")
     args = parser.parse_args()
     configs = json.load(open(args.configuration_file))
+    external_path = os.getenv("EXTERNAL_PATH")
     print("EXTERNAL_PATH is")
-    print(os.getenv("EXTERNAL_PATH"))
+    print(external_path)
     print("Configuration is")
     print(configs)
 
     for config in configs:
-        bagpath = os.getenv("EXTERNAL_PATH") + "/" + config["datapath"]
+        bagpath = external_path + "/" + config["datapath"]
         file_list = os.listdir(bagpath)
         file_list = sorted([file for file in file_list if file[-4:] == ".bag"])
         assert len(file_list) > 0, "Number of bags in the data folder, \033[91m{}\033[0m should be non-zero".format(
@@ -42,23 +43,6 @@ if __name__ == "__main__":
         else:
             end_timestamp = abs_start_timestamp + config["end_time"]
 
-        tmux_config = open("tmux_localization.yaml.template", "r").read()
-        namespace = config["namespace"]
-        if namespace[0] == "/":
-            namespace = namespace[1:]
-        if namespace[-1] == "/":
-            namespace = namespace[:-1]
-        tmux_config = tmux_config \
-            .replace("DATA_PATH", "/media/drive/" + config["datapath"]) \
-            .replace("RESULT_PATH", "/media/drive/" + config["outputpath"]) \
-            .replace("NAMESPACE", namespace) \
-            .replace("OPTIONAL_DURATION", "" if config["start_time"] == -1 else "--duration " + str(end_timestamp - start_timestamp)) \
-            .replace("DURATION", str(math.ceil(end_timestamp - start_timestamp) + 40)) \
-            .replace("OPTIONAL_START_TIME", "" if config["start_time"] == 0 else "-s " + str(config["start_time"]))
-        tmuxp_config_file = open("tmux_localization.yaml", "w")
-        tmuxp_config_file.write(tmux_config)
-        tmuxp_config_file.close()
-
         ugv_config = open("ugv.yaml.template", "r").read()
         ugv_config = ugv_config \
             .replace("RESULT_PATH", "/media/drive/" + config["outputpath"])
@@ -74,11 +58,24 @@ if __name__ == "__main__":
         ugv_launch_file.write(ugv_launch)
         ugv_launch_file.close()
 
-        proc = Process(target=escape_func, args=[math.ceil(end_timestamp - start_timestamp) + 90])
+        proc = Process(target=escape_func, args=[math.ceil(end_timestamp - start_timestamp) + 70])
         proc.start()
+
+        namespace = config["namespace"]
+        if namespace[0] != "/":
+            namespace = "/" + namespace
+        if namespace[-1] == "/":
+            namespace = namespace[:-1]
+
+        os.environ["DATA_PATH"] = "/media/drive/" + config["datapath"]
+        os.environ["RESULT_PATH"] = "/media/drive/" + config["outputpath"]
+        os.environ["NAMESPACE"] = namespace
+        os.environ["DURATION"] = str(math.ceil(end_timestamp - start_timestamp) + 40)
+        os.environ["START_TIME"] = str(config["start_time"])
+        os.environ["PLAY_DURATION"] = "65536" if config["start_time"] == -1 else str(end_timestamp - start_timestamp)
 
         os.system("tmuxp load tmux_localization.yaml")
 
-        proc.join()
+        os.environ.pop("MYVAR", None)
 
-        os.system("rm tmux_localization.yaml")
+        proc.join()
